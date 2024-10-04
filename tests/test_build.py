f13f8f79dcbaa17e885204d76f202f1e6d681db1
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-04 19:24:05 +0100

    rm rootdir

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,4 +1,5 @@
 import shutil
+from pathlib import Path
 from unittest import mock
 
 from sphinx.testing.util import SphinxTestApp
@@ -15,8 +16,9 @@ def request_session_head(url, **kwargs):
     'sphinx.builders.linkcheck.requests.head',
     side_effect=request_session_head,
 )
-def test_build_all(requests_head, tmp_path, rootdir):
-    shutil.copytree(rootdir / 'test-root', tmp_path, dirs_exist_ok=True)
+def test_build_all(requests_head, tmp_path):
+    test_root = Path(__file__).parent.resolve() / 'roots' / 'test-root'
+    shutil.copytree(test_root, tmp_path, dirs_exist_ok=True)
     app = SphinxTestApp('linkcheck', srcdir=tmp_path)
     for _ in range(1_000):
         app.build()
