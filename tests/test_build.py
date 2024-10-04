7dd64dcf87134809cb93c0b5d3960b8e2c3dd2bf
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-04 20:42:31 +0100

    inline ratelimit

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,35 +1,16 @@
 from __future__ import annotations
 
-import contextlib
-import time
+from collections import namedtuple
 from queue import PriorityQueue, Queue
 from pathlib import Path
 from threading import Thread
-from typing import NamedTuple
-from urllib.parse import urlsplit
 from unittest import mock
 
 from sphinx.util import requests
 
 CHECK_IMMEDIATELY = 0
 QUEUE_POLL_SECS = 1
-
-
-class CheckRequest(NamedTuple):
-    next_check: float
-    hyperlink: str | None
-
-
-class CheckResult(NamedTuple):
-    uri: str
-    status: str
-    message: str
-    code: int
-
-
-class RateLimit(NamedTuple):
-    delay: float
-    next_check: float
+CheckResult = namedtuple('CheckResult', ['uri', 'status', 'message', 'code'])
 
 
 TEST_ROOT = Path(__file__).parent.resolve() / 'test-root'
@@ -46,13 +27,7 @@ HYPERLINKS = [
 class HyperlinkAvailabilityCheckWorker(Thread):
     """A worker class for checking the availability of hyperlinks."""
 
-    def __init__(
-        self,
-        rqueue: Queue[CheckResult],
-        wqueue: Queue[CheckRequest],
-        rate_limits: dict[str, RateLimit],
-    ) -> None:
-        self.rate_limits = rate_limits
+    def __init__(self, rqueue: Queue[CheckResult], wqueue: Queue[str | None]) -> None:
         self.rqueue = rqueue
         self.wqueue = wqueue
         self._session = requests._Session()
@@ -60,26 +35,12 @@ class HyperlinkAvailabilityCheckWorker(Thread):
 
     def run(self) -> None:
         while True:
-            next_check, uri = self.wqueue.get()
+            uri = self.wqueue.get()
             if uri is None:
                 # An empty hyperlink is a signal to shutdown the worker; cleanup resources here
                 self._session.close()
                 break
 
-            netloc = urlsplit(uri).netloc
-            with contextlib.suppress(KeyError):
-                # Refresh rate limit.
-                # When there are many links in the queue, workers are all stuck waiting
-                # for responses, but the builder keeps queuing. Links in the queue may
-                # have been queued before rate limits were discovered.
-                next_check = self.rate_limits[netloc].next_check
-            if next_check > time.time():
-                # Sleep before putting message back in the queue to avoid
-                # waking up other threads.
-                time.sleep(QUEUE_POLL_SECS)
-                self.wqueue.put(CheckRequest(next_check, uri), False)
-                self.wqueue.task_done()
-                continue
             status, info, code = self._check_uri(uri)
             self.rqueue.put(CheckResult(uri, status, info, code))
             self.wqueue.task_done()
@@ -118,10 +79,6 @@ class HyperlinkAvailabilityCheckWorker(Thread):
             # that the link is broken.
             return 'broken', error_message, 0
 
-        # Success; clear rate limits for the origin
-        netloc = urlsplit(req_url).netloc
-        self.rate_limits.pop(netloc, None)
-
         if response_url.rstrip('/') == req_url.rstrip('/'):
             return 'working', '', 0
         elif redirect_status_code is not None:
@@ -146,22 +103,21 @@ def test_build_all(requests_head):
         print(f'loop: {i}')
 
         # setup
-        rate_limits: dict[str, RateLimit] = {}
         rqueue: Queue[CheckResult] = Queue()
         workers: list[HyperlinkAvailabilityCheckWorker] = []
-        wqueue: PriorityQueue[CheckRequest] = PriorityQueue()
+        wqueue: PriorityQueue[str | None] = PriorityQueue()
 
         # invoke threads
         num_workers = 5
         for _i in range(num_workers):
-            thread = HyperlinkAvailabilityCheckWorker(rqueue, wqueue, rate_limits)
+            thread = HyperlinkAvailabilityCheckWorker(rqueue, wqueue)
             thread.start()
             workers.append(thread)
 
         # check
         total_links = 0
         for hyperlink in HYPERLINKS:
-            wqueue.put(CheckRequest(CHECK_IMMEDIATELY, hyperlink), False)
+            wqueue.put(hyperlink, False)
             total_links += 1
 
         done = 0
@@ -173,4 +129,4 @@ def test_build_all(requests_head):
         # shutdown_threads
         wqueue.join()
         for _worker in workers:
-            wqueue.put(CheckRequest(CHECK_IMMEDIATELY, None), False)
+            wqueue.put(None, False)
