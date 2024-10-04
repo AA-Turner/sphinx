7e754fae2092d7a261c599293706f683f877c8d0
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-04 17:59:23 +0100

    use same app

--- tests/test_builders/test_build.py
+++ tests/test_builders/test_build.py
@@ -30,25 +30,6 @@ def nonascii_srcdir(request, rootdir, sphinx_test_tempdir):
     if not srcdir.exists():
         shutil.copytree(rootdir / 'test-root', srcdir)
 
-    # add a doc with a non-ASCII file name to the source dir
-    (srcdir / (test_name + '.txt')).write_text(
-        """
-nonascii file name page
-=======================
-""",
-        encoding='utf8',
-    )
-
-    root_doc = srcdir / 'index.txt'
-    root_doc.write_text(
-        root_doc.read_text(encoding='utf8')
-        + f"""
-.. toctree::
-
-{test_name}/{test_name}
-""",
-        encoding='utf8',
-    )
     return srcdir
 
 
@@ -56,7 +37,7 @@ nonascii file name page
     'sphinx.builders.linkcheck.requests.head',
     side_effect=request_session_head,
 )
-def test_build_all(make_app, nonascii_srcdir):
+@pytest.mark.sphinx('linkcheck')
+def test_build_all(requests_head, app):
     for _ in range(1_000):
-        app = make_app('linkcheck', srcdir=nonascii_srcdir)
         app.build()
