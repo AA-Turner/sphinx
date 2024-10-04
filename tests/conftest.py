5389a1bccb5f325f0f57a2bf6fd42e392de62ec6
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-04 20:59:37 +0100

    rm test-root

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,7 @@
-from __future__ import annotations
-
 import sys
 
-import docutils
-import pytest
 
-import sphinx
-import sphinx.locale
-import sphinx.pycode
-
-pytest_plugins = ['sphinx.testing.fixtures']
-
-# Exclude 'roots' dirs for pytest test collector
-collect_ignore = ['test-root']
-
-
-def pytest_report_header(config: pytest.Config) -> str:
-    header = f'libraries: Sphinx-{sphinx.__display_version__}, docutils-{docutils.__version__}'
+def pytest_report_header(config) -> str:
     if sys.version_info[:2] >= (3, 13):
-        header += f'\nGIL enabled?: {sys._is_gil_enabled()}'
-    if hasattr(config, '_tmp_path_factory'):
-        header += f'\nbase tmp_path: {config._tmp_path_factory.getbasetemp()}'
-    return header
+        return f'\nGIL enabled?: {sys._is_gil_enabled()}'
+    return ''
