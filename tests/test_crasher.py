d98138a7d4de36fa17040b9aa0a2999cbb610c00
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-05 23:26:38 +0100

    run

--- tests/test_crasher.py
+++ tests/test_crasher.py
@@ -4,5 +4,14 @@ class Baz:
 
 
 def test_crasher():
+    assert Baz.__new__.__annotations__ == {}
     obj = Baz.__dict__['__new__']
-    annotations = getattr(obj, '__annotations__', None)
+    obj.__annotations__
+    getattr(obj, '__annotations__', None)
+
+
+if __name__ == '__main__':
+    assert Baz.__new__.__annotations__ == {}
+    obj = Baz.__dict__['__new__']
+    obj.__annotations__
+    getattr(obj, '__annotations__', None)
