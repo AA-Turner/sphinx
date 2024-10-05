df52df08230e22afb8fdd011924719b752b93e37
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-05 23:11:16 +0100

    no if

--- tests/test_ext_autodoc_configs.py
+++ tests/test_ext_autodoc_configs.py
@@ -2,7 +2,9 @@
 
 from __future__ import annotations
 
-from sphinx.util import inspect
+import inspect
+
+from sphinx.util.inspect import isabstractmethod, iscoroutinefunction
 
 
 class Baz:
@@ -12,10 +14,6 @@ class Baz:
 
 def test_autodoc_class_signature_separated_new():
     obj = Baz.__dict__['__new__']
-    if inspect.isabstractmethod(obj):
-        pass
-    if inspect.iscoroutinefunction(obj) or inspect.isasyncgenfunction(obj):
-        pass
-    if (inspect.isclassmethod(obj) or
-            inspect.is_singledispatch_method(obj) and inspect.isclassmethod(obj.func)):
-        pass
+    isabstractmethod(obj)
+    iscoroutinefunction(obj)
+    inspect.isasyncgenfunction(obj)
