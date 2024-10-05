e5e7168cb4515f2817935936d56b6f84713713fb
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-05 23:22:45 +0100

    only annotations

--- tests/test_crasher.py
+++ tests/test_crasher.py
@@ -5,8 +5,4 @@ class Baz:
 
 def test_crasher():
     obj = Baz.__dict__['__new__']
-    name = getattr(obj, '__name__', None)
-    code = getattr(obj, '__code__', None)
-    defaults = getattr(obj, '__defaults__', None)
-    kwdefaults = getattr(obj, '__kwdefaults__', None)
     annotations = getattr(obj, '__annotations__', None)
