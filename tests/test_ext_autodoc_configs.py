73844e7b8726752712aab2c97d0b3698e10d96b3
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-05 23:00:32 +0100

    Revert "minify"
    This reverts commit 09867ab2ae12920dfd19c6c0bf09af1af0d36b39.

---
+++ tests/test_ext_autodoc_configs.py
@@ -0,0 +1,57 @@
+"""Test the autodoc extension.  This tests mainly for config variables"""
+
+from __future__ import annotations
+
+from unittest.mock import Mock
+
+import pytest
+
+# NEVER import those objects from sphinx.ext.autodoc directly
+from sphinx.ext.autodoc.directive import DocumenterBridge, process_documenter_options
+from sphinx.util import inspect
+from sphinx.util.docutils import LoggingReporter
+
+
+class Baz:
+    def __new__(cls, x, y):
+        pass
+
+
+@pytest.mark.sphinx('html', testroot='ext-autodoc')
+def test_autodoc_class_signature_separated_new(app):
+    obj = Baz.__new__
+    if inspect.isabstractmethod(obj):
+        pass
+    if inspect.iscoroutinefunction(obj) or inspect.isasyncgenfunction(obj):
+        pass
+    if (inspect.isclassmethod(obj) or
+            inspect.is_singledispatch_method(obj) and inspect.isclassmethod(obj.func)):
+        pass
+
+
+    app.config.autodoc_class_signature = 'separated'
+    options = {
+        'members': None,
+        'undoc-members': None,
+    }
+    app.env.temp_data.setdefault('docname', 'index')  # set dummy docname
+    doccls = app.registry.documenters['class']
+    docoptions = process_documenter_options(doccls, app.config, options)
+    state = Mock()
+    state.document.settings.tab_width = 8
+    bridge = DocumenterBridge(app.env, LoggingReporter(''), docoptions, 1, state)
+    documenter = doccls(bridge, 'target.classes.Baz')
+    documenter.generate()
+    actual = bridge.result
+
+    assert list(actual) == [
+        '',
+        '.. py:class:: Baz',
+        '   :module: target.classes',
+        '',
+        '',
+        '   .. py:method:: Baz.__new__(cls, x, y)',
+        '      :module: target.classes',
+        '      :staticmethod:',
+        '',
+    ]
