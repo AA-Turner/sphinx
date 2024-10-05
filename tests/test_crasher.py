dd272d212037ac0666fd80e2f528d2b7d7231086
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-10-05 23:28:21 +0100

    staticmethod

--- tests/test_crasher.py
+++ tests/test_crasher.py
@@ -3,15 +3,9 @@ class Baz:
         pass
 
 
-def test_crasher():
-    assert Baz.__new__.__annotations__ == {}
-    obj = Baz.__dict__['__new__']
-    obj.__annotations__
-    getattr(obj, '__annotations__', None)
-
-
 if __name__ == '__main__':
     assert Baz.__new__.__annotations__ == {}
     obj = Baz.__dict__['__new__']
-    obj.__annotations__
+    assert isinstance(obj, staticmethod)
     getattr(obj, '__annotations__', None)
+    obj.__annotations__
