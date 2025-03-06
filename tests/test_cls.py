ba273298bb5bf52b1a58e5429c7e13ede7c6dbfb
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-03-06 15:46:57 +0000

    test

--- tests/test_cls.py
+++ tests/test_cls.py
@@ -1,39 +1,19 @@
 from __future__ import annotations
 
 import sys
-from pathlib import Path
+import types
 from typing import Union
 
 import pytest
 
 
-class Foo:
-    pass
-
-
-class _Baz:
-    pass
-
-
-class Exc(Exception):
-    pass
-
-
-class _Exc(Exception):
-    pass
-
-
 def test_class():
-    assert isinstance(Path, type)
-    assert not issubclass(Path, BaseException)
+    assert isinstance(str | int, types.UnionType)
     if sys.version_info >= (3, 14, 0, 'alpha', 5):
         assert isinstance(Union, type)
         assert not issubclass(Union, BaseException)
+        assert isinstance(str | int, Union)
     else:
         assert not isinstance(Union, type)
         with pytest.raises(TypeError):
             issubclass(Union, BaseException)
-    assert isinstance(Foo, type)
-    assert not issubclass(Foo, BaseException)
-    assert isinstance(_Baz, type)
-    assert not issubclass(_Baz, BaseException)
