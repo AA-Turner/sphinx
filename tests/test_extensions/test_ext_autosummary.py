beba9d54cb14bde5045e12be52338ab19dc21251
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-03-06 13:57:00 +0000

    no documenter

--- tests/test_extensions/test_ext_autosummary.py
+++ tests/test_extensions/test_ext_autosummary.py
@@ -5,46 +5,13 @@ from __future__ import annotations
 import sys
 import types
 
-from sphinx.ext.autodoc import (
-    AttributeDocumenter,
-    ClassDocumenter,
-    DataDocumenter,
-    DecoratorDocumenter,
-    ExceptionDocumenter,
-    FunctionDocumenter,
-    MethodDocumenter,
-    ModuleDocumenter,
-    PropertyDocumenter,
-)
-from sphinx.ext.autosummary import FakeDirective
-
 from tests.conftest import _TESTS_ROOT
 
 sys.path.insert(0, str(_TESTS_ROOT / 'roots/test-ext-autosummary'))
 
-DOCUMENTERS = (
-    ModuleDocumenter,
-    ClassDocumenter,
-    ExceptionDocumenter,
-    DataDocumenter,
-    FunctionDocumenter,
-    MethodDocumenter,
-    AttributeDocumenter,
-    DecoratorDocumenter,
-    PropertyDocumenter,
-)
-
 
 def _is_class_documenter(obj):
-    if isinstance(obj, types.ModuleType):
-        # ModuleDocumenter.can_document_member always returns False
-        return False
-
-    # Construct a fake documenter for *parent*
-    parent_doc = ModuleDocumenter(FakeDirective(), '')
-
-    # Get the correct documenter class for *obj*
-    return ClassDocumenter.can_document_member(obj, '', False, parent_doc)
+    return isinstance(obj, type)
 
 
 def test_autosummary_generate_content_for_module_imported_members():
